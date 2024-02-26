e36169cbcee57853e9bf7850c8728c3d096b15bc
Frederik H. Sørensen <72657308+GoosLife@users.noreply.github.com>
2024-02-26 18:05:49 +0100

    Add project files.

---
+++ HashKeyGenerator/Hasher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace HashKeyGenerator
+{
+    internal static class Hasher
+    {
+        public static byte[] GenerateHash(string input, HMAC algorithm)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+
+            byte[] hash = algorithm.ComputeHash(bytes);
+
+            return hash;
+        }
+
+        public static bool VerifyHash(string input, byte[] hash, HMAC algorithm)
+        {
+            byte[] newHash = GenerateHash(input, algorithm);
+
+            return newHash.SequenceEqual(hash);
+        }
+
+        public static string WriteHashHex(byte[] hash)
+        {
+            // Print the hash in hexadecimal format
+            StringBuilder hex = new StringBuilder(hash.Length * 2);
+            foreach (byte b in hash)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+            return hex.ToString();
+        }
+
+        public static string WriteHashASCII(byte[] hash)
+        {
+            // Print the hash in ASCII format
+            StringBuilder ascii = new StringBuilder(hash.Length);
+
+            foreach (byte b in hash)
+            {
+                ascii.Append((char)b);
+            }
+
+            return ascii.ToString();
+        }
+    }
+}
---
+++ HashKeyGenerator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Cryptography;
+
+namespace HashKeyGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] algorithms = new string[] { "HMACSHA1", "HMACMD5", "HMACSHA256", "HMACSHA384", "HMACSHA512" };
+
+            Console.WriteLine("Choose a hashing algorithm: ");
+
+            for (int i = 0; i < algorithms.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {algorithms[i]}");
+            }
+
+            int choice = int.Parse(Console.ReadLine());
+
+            HMAC? algorithm = choice switch
+            {
+                1 => new HMACSHA1(),
+                2 => new HMACMD5(),
+                3 => new HMACSHA256(),
+                4 => new HMACSHA384(),
+                5 => new HMACSHA512(),
+                _ => null
+            };
+
+            if (algorithm == null)
+            {
+                Console.WriteLine("You failed to choose an algorithm (loop not implemented).\nPress any key to exit...");
+                return;
+            }
+
+            Console.WriteLine("Enter a string to hash: ");
+            string input = Console.ReadLine();
+
+            byte[] hash = Hasher.GenerateHash(input, algorithm);
+            Console.WriteLine("HEX:\t" + Hasher.WriteHashHex(hash));
+            Console.WriteLine("ASCII:\t" + Hasher.WriteHashASCII(hash));
+
+            // Test the hash
+            Console.WriteLine("IS VALID: " + Hasher.VerifyHash(input, hash, algorithm));
+
+            // Test for a random collision, just to demonstrate that the hash is unique
+            byte[] randomHash = Hasher.GenerateHash(Guid.NewGuid().ToString(), algorithm);
+            Console.WriteLine("IS UNIQUE: " + !Hasher.VerifyHash(input, randomHash, algorithm));
+
+            Console.WriteLine("Press any key to exit...");
+        }
+    }
+}
